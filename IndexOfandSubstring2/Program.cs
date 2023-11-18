b3dadd3ca89a141046bc0977ff6e72d85fda2548
alvaro-costa <alv.cav@hotmail.com>
2023-11-18 09:48:51 -0300

    Adicionado novo projeto IndexOfandSubstring

---
+++ IndexOfandSubstring2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Um programa para estudo de comportamento dos métodos IndexOf() e Substring()
+ * 
+ * A program to study the behavior of IndexOf() and Substring() methods.
+ * Author: Alvaro Costa
+ */
+
+
+using System;
+
+namespace IndexOfandSubstring2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa de estudo dos métodos IndexOf() e Substring()");
+
+        }
+    }
+}
+
