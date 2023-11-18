a2d37ecf932b4ecacc063a996de4a856f0fecb7f
alvaro-costa <alv.cav@hotmail.com>
2023-11-18 08:51:08 -0300

    Revert "Adicionado estrutura inicial em Program.cs"
    This reverts commit 9d7899c20bc405bfd96669b04665cf56b12c93b6.

--- IndexOfandSubstring/Program.cs
+++ IndexOfandSubstring/Program.cs
@@ -1,13 +1,1 @@
-﻿using System;
-
-namespace IndexOfAndSubstring
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Resumo sobre IndexOf e Substring");
-        }
-    }
-}
-
+﻿Console.WriteLine("Resumo sobre IndexOf e Substring");
