9d7899c20bc405bfd96669b04665cf56b12c93b6
alvaro-costa <alv.cav@hotmail.com>
2023-11-18 08:49:09 -0300

    Adicionado estrutura inicial em Program.cs

--- IndexOfandSubstring/Program.cs
+++ IndexOfandSubstring/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Resumo sobre IndexOf e Substring");
+﻿using System;
+
+namespace IndexOfAndSubstring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Resumo sobre IndexOf e Substring");
+        }
+    }
+}
+
