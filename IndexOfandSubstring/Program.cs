b8b4912558c40880b19f79c59ec7e1d61fbce75b
alvaro-costa <alv.cav@hotmail.com>
2023-11-18 09:00:55 -0300

    Adicionado comentario proposito no Program.cs

--- IndexOfandSubstring/Program.cs
+++ IndexOfandSubstring/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * This programs is a study of two methods: IndexOf() and Substring()
+ * Use it to analyse the behavior of these thow elements.
+ * Author: Alvaro Costa
+ * Date of creation: 11/18/2023
+ */
+
+using System;
 
 namespace IndexOfandSubstring
 {
@@ -7,7 +14,6 @@ namespace IndexOfandSubstring
         static void Main(string[] args)
         {
             Console.WriteLine("Resumo sobre IndexOf e Substring");
-
         }
     }
 }
