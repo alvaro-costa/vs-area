76b63be5aa8a7ffcf7d8e4696dbd5fb48367ed07
alvaro-costa <alv.cav@hotmail.com>
2023-11-18 23:57:14 -0300

    Criacao de Projeto para estudo do IndexOfAny e LastIndexOf

---
+++ IndexOfAnyandLastIndexOf/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Estudo dos métodos IndexOfAny() e LastIndexOf()
+ * 
+ */
+
+using System;
+
+
+
+namespace IndexOfAnyandLastIndexOf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
