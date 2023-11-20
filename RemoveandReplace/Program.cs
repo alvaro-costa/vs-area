12160e38f1408eeb1966e480a5deb73445772ed9
alvaro-costa <alv.cav@hotmail.com>
2023-11-20 08:19:03 -0300

    Estudo e desafio do Remove() e Replace()

---
+++ RemoveandReplace/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace RemoveandReplace
+{
+
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            /*
+             * Casos de exemplo:
+             */
+
+            //string data = "12345John Smith          5000  3  ";
+            //string updatedData = data.Remove(5, 20);
+            //Console.WriteLine(updatedData);
+
+            //string message = "This--is--ex-amp-le--da-ta";
+            //message = message.Replace("--", " ");
+            //message = message.Replace("-", "");
+            //Console.WriteLine(message);
+
+            /*
+             * Aplicação:
+             */
+
+            const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+            string quantity = "";
+            string output = "";
+
+            const string tradeMark = "&trade";
+            const string regMark = "&reg";
+
+
+            // Isolating <div> context.
+
+            const string startOutputTag = "<div>";
+            const string endOutputTag = "</div>";
+
+            int startOutputIndex = input.IndexOf(startOutputTag) + startOutputTag.Length;
+            int endOutputIndex = input.IndexOf(endOutputTag);
+            int lengthOutput = endOutputIndex - startOutputIndex;
+
+            output = input.Substring(startOutputIndex, lengthOutput);
+            output = output.Replace(tradeMark, regMark);
+
+
+            // Isolating <span> context.
+
+            const string startQuantityTag = "<span>";
+            const string endQuantityTag = "</span>";
+
+            int startQuantityIndex = input.IndexOf(startQuantityTag) + startQuantityTag.Length;
+            int endQuantityIndex = input.IndexOf(endQuantityTag);
+            int lengthQuantity = endQuantityIndex - startQuantityIndex;
+
+            quantity = input.Substring(startQuantityIndex, lengthQuantity);
+
+
+            // Result
+
+            Console.WriteLine(quantity);
+            Console.WriteLine(output);
+        }
+    }
+}
